7cb01acddba2af35672b63210e8ebeb32d9f3824
David Katsir <david.katsir@gmail.com>
2023-10-02 02:14:04 +0300

    Commit orderDigitsDescending

--- TypeScript/CodeWars/Kyu/kyu7Exercises.ts
+++ TypeScript/CodeWars/Kyu/kyu7Exercises.ts
@@ -1,30 +1,47 @@
 // Question:
+// Get a number as input and order it in descending order
+// If the input would be: 52819, then the output should be: 98521
 
+function orderDigitsDescending(inputNumber: number) {
+  if (inputNumber < 0) {
+    inputNumber = inputNumber * -1;
+  }
+  const digitsArray: number[] = [];
+  while (inputNumber > 0) {
+    let digit = inputNumber % 10;
+    digitsArray.push(digit);
+    inputNumber = Math.floor(inputNumber / 10);
+  }
+  digitsArray.sort((a, b) => b - a);
 
+  return digitsArray.join("");
+}
+
+const exampleNumber01 = 13246252;
+const exampleNumber02 = 1596;
+const exampleNumber03 = -62541;
+
+console.log(orderDigitsDescending(exampleNumber01)); // Output = 654321
+console.log(orderDigitsDescending(exampleNumber02)); // Output = 9651
+console.log(orderDigitsDescending(exampleNumber03)); // Output = 65421
 
 endOfFunction();
 //******************************************************************************************************** */
 
-
-
-
-
-
-
 function endOfFunction() {
-    console.log(
-      "                                                                "
-    );
-    console.log(
-      "****************************************************************"
-    );
-    console.log(
-      "**********             End of question            **************"
-    );
-    console.log(
-      "****************************************************************"
-    );
-    console.log(
-      "                                                                "
-    );
-  }
+  console.log(
+    "                                                                "
+  );
+  console.log(
+    "****************************************************************"
+  );
+  console.log(
+    "**********             End of question            **************"
+  );
+  console.log(
+    "****************************************************************"
+  );
+  console.log(
+    "                                                                "
+  );
+}
