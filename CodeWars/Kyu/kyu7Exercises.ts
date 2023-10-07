f7855c64a57305dc0c50e6a606e61b1e6091d4fb
David Katsir <david.katsir@gmail.com>
2023-10-08 00:09:07 +0300

    Commit isIsogram

--- CodeWars/Kyu/kyu7Exercises.ts
+++ CodeWars/Kyu/kyu7Exercises.ts
@@ -282,6 +282,31 @@ export default function isSquare(n: number): boolean {
 
 endOfFunction();
 //******************************************************************************************************** */
+// Isogram
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a
+// function that determines whether a string that contains only letters is an isogram.
+// Assume the empty string is an isogram. Ignore letter case.
+// Example: (Input --> Output)
+// "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+// isIsogram "Dermatoglyphics" = true
+// isIsogram "moose" = false
+// isIsogram "aba" = false
+
+export function isIsogram(str: string): boolean {
+  const strLowerCase = str.toLowerCase();
+  const strToArray: string[] = strLowerCase.split("");
+  for (let i = 0; i < strToArray.length; i++) {
+    for (let y = i + 1; y < strToArray.length; y++) {
+      if (strToArray[i] === strToArray[y]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+endOfFunction();
+//******************************************************************************************************** */
 
 
 endOfFunction();
