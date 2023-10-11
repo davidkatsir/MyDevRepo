ede4a0f9937f0f7b6b92cc90f4133ec0548f3cef
David Katsir <david.katsir@gmail.com>
2023-10-11 18:31:14 +0300

    commit 11-10-2023

--- CodeWars/Kyu/kyu7Exercises.ts
+++ CodeWars/Kyu/kyu7Exercises.ts
@@ -339,6 +339,59 @@ export function xo(str: string): boolean {
 
 endOfFunction();
 //******************************************************************************************************** */
+// Jaden Casing Strings
+// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+// Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+// Example:
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+
+String.prototype.toJadenCase = function () {
+  // Split the string into words
+  const words = this.split(' ');
+
+  // Capitalize the first letter of each word
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+  }
+
+  // Join the words back together with spaces
+  return words.join(' ');
+};
+
+// Extend the String interface to declare the new method
+interface String {
+  toJadenCase(): string;
+}
+
+// Now you can use the toJadenCase method on any string
+const exampleString = "this is a test";
+console.log(exampleString.toJadenCase()); // Outputs: "This Is A Test"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
