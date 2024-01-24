adaf219bb63091b6d031299ccba7368f07c15b2d
David Katsir <david.katsir@gmail.com>
2024-01-24 15:24:18 +0200

    Commit 24-01-2024

--- CodeWars/Kyu/TypeScriptExcercises/kyu7Exercises.ts
+++ CodeWars/Kyu/TypeScriptExcercises/kyu7Exercises.ts
@@ -745,9 +745,10 @@ endOfFunction();
 // Take a look on the test cases.
 // Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the returned integer can't be negative.
 // The second value in the first pair in the array is 0, since the bus is empty in the first bus stop.
-export function number(busStops: [number, number][]): number {
-  // Your Code
-}
+
+// export function number(busStops: [number, number][]): number {
+//   // Your Code
+// }
 
 endOfFunction();
 //******************************************************************************************************** */
